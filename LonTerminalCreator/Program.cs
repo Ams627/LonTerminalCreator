3fc9d45a6870f2befdbae0a401eb3e8aa50807f2
Adrian Sims <asims@parkeon.com>
2017-06-16 15:32:18 +0100

    corrected linq

--- LonTerminalCreator/Program.cs
+++ LonTerminalCreator/Program.cs
@@ -21,26 +21,48 @@ namespace LonTerminalCreator
 
                 var document = XDocument.Load(args[0]);
                 var ns = document.Root.GetDefaultNamespace();
-
-                var res = (from origin in document.Descendants(ns + "Station")
+                var doubleDict = (from origin in document.Descendants(ns + "Station")
+                           let originCrs = origin.Attribute("Crs")?.Value
                            let londonList = origin.Elements(ns + "FareGroupLocation")
                                 ?.Where(y => y.Attribute("Nlc")?.Value == "1072")
-                                ?.FirstOrDefault()?.Descendants(ns + "Station").Select(d=>d.Value)?.ToList()
-                           let originCrs = origin.Attribute("Crs")?.Value
-                           where originCrs != null && londonList != null
-                           select new {originCrs, londonList}
-                           ).ToDictionary(x => x.originCrs, x => x.londonList);
-                          
-                foreach (var station in res)
+                           where originCrs != null && londonList != null && londonList.Count() > 0
+                           select new
+                           {
+                               originCrs,
+                               routeDic = (from routeSpec in londonList
+                                           select new
+                                           {
+                                               routeCode = routeSpec.Element("FareRoute")?.Attribute("Code")?.Value,
+                                               permittedSpec = new
+                                               {
+                                                   startDate = routeSpec?.Element("FareRoute")?.Element("PermittedStations")?.Attribute("StartDate"),
+                                                   endDate = routeSpec?.Element("FareRoute")?.Element("PermittedStations")?.Attribute("EndDate"),
+                                                   stationList = routeSpec?.Element("FareRoute")?.Element("PermittedStations")?.Elements("Station").Select(x => x?.Value)
+                                               }
+                                           }).ToDictionary(x => x.routeCode, x => x.permittedSpec)
+                           }).ToDictionary(x => x.originCrs, x => x.routeDic);
+
+                var res = doubleDict.TryGetValue("POO", out var pooleRoutes);
+                if (res)
                 {
-                    Console.WriteLine($"{station.Key}");
-                    foreach (var destination in station.Value)
-                    {
-                        Console.WriteLine($"    {destination}");
-                    }
+                    var res2 = pooleRoutes.TryGetValue("00000", out var pooleStation);
+                    Console.WriteLine("");
                 }
 
-                Console.WriteLine("");
+                foreach (var station in doubleDict)
+                {
+                    Console.WriteLine($"{station.Key}");
+                    foreach (var routedic in station.Value)
+                    {
+                        Console.WriteLine($"    Route: {routedic.Key}");
+                        Console.WriteLine($"    StartDate: {routedic.Value.startDate}");
+                        Console.WriteLine($"    Route: {routedic.Value.endDate}");
+                        foreach (var destination in routedic.Value.stationList)
+                        {
+                            Console.WriteLine($"        {destination}");
+                        }
+                    }
+                }
             }
             catch (Exception ex)
             {
