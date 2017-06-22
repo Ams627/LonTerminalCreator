bbb8315ef2bada0742f9d060ea588aeeba88e611
Ams <adriansims62781@gmail.com>
2017-06-22 23:40:52 +0100

    Got Linq right!

--- LonTerminalCreator/Program.cs
+++ LonTerminalCreator/Program.cs
@@ -22,52 +22,81 @@ namespace LonTerminalCreator
                 var document = XDocument.Load(args[0]);
                 var ns = document.Root.GetDefaultNamespace();
 
-                //var countDic = (from origin in document.Descendants(ns + "Station")
-                //                let originCrs = origin.Attribute("Crs")?.Value;
+
+                var d = document.Descendants(ns + "Stations").First().Elements(ns + "Station")
+                    .GroupBy(x => x.Attribute("Crs")?.Value,
+                             x => x.Elements("FareGroupLocation").Where(y => y.Attribute("Nlc")?.Value == "1072")  // take only london dests
+                                    .SelectMany(z => z.Elements("FareRoute")).
+                                        GroupBy(a => a.Attribute("Code"),       // group by route code 
+                                                i => new
+                                                {
+                                                    StartDate = i.Element("PermittedStations")?.Attribute("StartDate")?.Value,
+                                                    EndDate = i.Element("PermittedStations")?.Attribute("EndDate")?.Value,
+                                                    StationList = i.Element("PermittedStations").Elements("Station").Select(j => j.Value).ToList()
+                                                }));
+
+                var crsDups = d.Where(x => x.Count() > 1);
 
 
-                var doubleDict = (from origin in document.Descendants(ns + "Station")
-                           let originCrs = origin.Attribute("Crs")?.Value
-                           let londonList = origin.Elements(ns + "FareGroupLocation")
-                                ?.Where(y => y.Attribute("Nlc")?.Value == "1072")
-                           where originCrs != null && londonList != null && londonList.Count() > 0
-                           select new
-                           {
-                               originCrs,
-                               routeDic = (from routeSpec in londonList
-                                           select new
-                                           {
-                                               routeCode = routeSpec.Element("FareRoute")?.Attribute("Code")?.Value,
-                                               permittedSpec = new
-                                               {
-                                                   startDate = routeSpec?.Element("FareRoute")?.Element("PermittedStations")?.Attribute("StartDate"),
-                                                   endDate = routeSpec?.Element("FareRoute")?.Element("PermittedStations")?.Attribute("EndDate"),
-                                                   stationList = routeSpec?.Element("FareRoute")?.Element("PermittedStations")?.Elements("Station").Select(x => x?.Value)
-                                               }
-                                           }).ToDictionary(x => x.routeCode, x => x.permittedSpec)
-                           }).ToDictionary(x => x.originCrs, x => x.routeDic);
-
-                var res = doubleDict.TryGetValue("POO", out var pooleRoutes);
-                if (res)
+                XNamespace xsi = XNamespace.Get("http://www.w3.org/2001/XMLSchema-instance");
+                XNamespace xsd = XNamespace.Get("http://www.w3.org/2001/XMLSchema");
+                XNamespace outputns = XNamespace.Get("http://schema.test.com/test");
+                var outputDoc = new XDocument(
+                        new XDeclaration("1.0", "utf-8", "yes"),
+                        new XElement(outputns + "LondonTerminalMappingData",
+                                            new XAttribute("xmlns", outputns.NamespaceName),
+                                            new XAttribute(XNamespace.Xmlns + "xsd", xsd.NamespaceName),
+                                            new XAttribute(XNamespace.Xmlns + "xsi", xsi.NamespaceName),
+                        new XElement(ns + "Stations",
+                            from station in d select new XElement(ns + "Station",
+                                new XElement(ns + "CrsCode", new XText(station.Key),
+                                    new XElement(ns + "LondonTerminalsMappings",
+                                        from route in station.First()
+                                            select new XElement(ns + "LondonTerminalMapping",
+                                                new XElement(ns + "LondonTerminals", 
+                                                    from dest in route.Key
+                                                    select new XElement(ns + "Terminal", new XText(dest.st                                                    
+                                                    
+                                                    
+                                                    )))));
+                outputDoc.Save("q:\\temp\\save1.xml");
+                foreach (var station in d)
                 {
-                    var res2 = pooleRoutes.TryGetValue("00000", out var pooleStation);
-                    Console.WriteLine("");
-                }
-
-                foreach (var station in doubleDict)
-                {
-                    Console.WriteLine($"{station.Key}");
-                    foreach (var routedic in station.Value)
+                    Console.WriteLine($"CRS {station.Key}");
+                    foreach(var stationElement in station) // should be only one station in each GroupBy grouping as we group by CRS
                     {
-                        Console.WriteLine($"    Route: {routedic.Key}");
-                        Console.WriteLine($"    StartDate: {routedic.Value.startDate}");
-                        Console.WriteLine($"    Route: {routedic.Value.endDate}");
-                        foreach (var destination in routedic.Value.stationList)
+                        foreach (var route in stationElement)
                         {
-                            Console.WriteLine($"        {destination}");
+                            Console.WriteLine($"{route.Key}");
+                            foreach (var permittedStationList in route)
+                            {
+                                Console.WriteLine($"start date is {permittedStationList.StartDate}");
+                                Console.WriteLine($"end date is {permittedStationList.EndDate}");
+                                foreach (var destination in permittedStationList.StationList)
+                                {
+                                    Console.WriteLine($"{destination}");
+                                }
+                            }
                         }
                     }
                 }
+
+
+
+                //foreach (var station in doubleDict)
+                //{
+                //    Console.WriteLine($"{station.Key}");
+                //    foreach (var routedic in station.Value)
+                //    {
+                //        Console.WriteLine($"    Route: {routedic.Key}");
+                //        Console.WriteLine($"    StartDate: {routedic.Value.startDate}");
+                //        Console.WriteLine($"    EndDate: {routedic.Value.endDate}");
+                //        foreach (var destination in routedic.Value.stationList)
+                //        {
+                //            Console.WriteLine($"        {destination}");
+                //        }
+                //    }
+                //}
             }
             catch (Exception ex)
             {
